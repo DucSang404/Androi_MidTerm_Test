0dc4e308cacae900db8029007d7da777d6f130fa
DucSang404 <ndsang404@gmail.com>
2025-03-20 13:36:12 +0700

    mssv-name

--- Androi/app/src/main/java/com/example/midterm_exam/LoginActivity.java
+++ Androi/app/src/main/java/com/example/midterm_exam/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.midterm_exam;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+//22110404 - Nguyễn Đức Sang
 
 public class LoginActivity extends AppCompatActivity {
     @Override
