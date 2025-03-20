5861d6251b99caba6719908d5a85d8fd50b803c2
truongsondev <sonltute@gmail.com>
2025-03-20 15:18:41 +0700

    activity otp

--- Androi/app/src/main/java/com/example/midterm_exam/service/ApiService.java
+++ Androi/app/src/main/java/com/example/midterm_exam/service/ApiService.java
@@ -1,9 +1,12 @@
 package com.example.midterm_exam.service;
 
 import com.example.midterm_exam.model.Account;
+import com.example.midterm_exam.model.ApiResponse;
+import com.example.midterm_exam.model.OtpRequest;
 import com.example.midterm_exam.model.User;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
@@ -15,4 +18,6 @@ public interface ApiService {
     @POST("api/v1/login")
     Call<Account> login(@Path("username") String username, @Path("password") String password);
 
+    @POST("api/verify-otp")
+    Call<ApiResponse> verifyOtp(@Body OtpRequest otpRequest);
 }
