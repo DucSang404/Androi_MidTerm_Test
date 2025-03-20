609711da8ac0346aaf1cb615c798bc10b09b227a
truongsondev <sonltute@gmail.com>
2025-03-20 16:47:36 +0700

    fix bug

--- Androi/app/src/main/java/com/example/midterm_exam/config/RetrofitClient.java
+++ Androi/app/src/main/java/com/example/midterm_exam/config/RetrofitClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit;
-    private static final String BASE_URL = "192.168.42.1:8080";
+    private static final String BASE_URL = "http://192.168.42.1:8080";
     
     public static Retrofit getRetrofitInstance() {
         if(retrofit == null) {
