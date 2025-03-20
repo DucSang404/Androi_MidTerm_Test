1a83f0a2ad7674ec4d400ace406fa4c99f560d3b
mylinh24 <hoangthimylinhbh2004@gmail.com>
2025-03-20 14:31:05 +0700

    22110363

--- Androi/app/src/main/java/com/example/midterm_exam/service/ApiService.java
+++ Androi/app/src/main/java/com/example/midterm_exam/service/ApiService.java
@@ -1,6 +1,9 @@
 package com.example.midterm_exam.service;
 
+import com.example.midterm_exam.model.Category;
 import com.example.midterm_exam.model.User;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -9,5 +12,6 @@ import retrofit2.http.Path;
 public interface ApiService {
     @GET("api/v1/users/{id}")
     Call<User> getUser(@Path("id") int userId);
+
 
 }
