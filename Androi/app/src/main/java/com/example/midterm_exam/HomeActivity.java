8e1d4c5b4c7e1107cd990042df705aa441e29c3a
ThuyLinh09 <TieuHoLy.09>
2025-03-20 14:22:24 +0700

    22110364 - Nguyen Hoang Thuy Linh

--- Androi/app/src/main/java/com/example/midterm_exam/HomeActivity.java
+++ Androi/app/src/main/java/com/example/midterm_exam/HomeActivity.java
@@ -2,23 +2,36 @@ package com.example.midterm_exam;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.menu);
+
+        // Mặc định hiển thị HomeFragment khi mở app
+        loadFragment(new HomeFragment());
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = new HomeFragment();
+
+            loadFragment(selectedFragment);
+            return true;
         });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.commit();
     }
 }
