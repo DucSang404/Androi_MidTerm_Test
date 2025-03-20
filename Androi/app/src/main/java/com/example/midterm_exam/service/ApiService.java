0c560a34e4dd3606fa65dc72288de352049726df
DucSang404 <ndsang404@gmail.com>
2025-03-20 14:23:52 +0700

    connect login to spring boot

--- Androi/app/src/main/java/com/example/midterm_exam/service/ApiService.java
+++ Androi/app/src/main/java/com/example/midterm_exam/service/ApiService.java
@@ -1,13 +1,18 @@
 package com.example.midterm_exam.service;
 
+import com.example.midterm_exam.model.Account;
 import com.example.midterm_exam.model.User;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface ApiService {
     @GET("api/v1/users/{id}")
     Call<User> getUser(@Path("id") int userId);
+
+    @POST("api/v1/login")
+    Call<Account> login(@Path("username") String username, @Path("password") String password);
 
 }
